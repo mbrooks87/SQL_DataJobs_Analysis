9ae8d116d0a23dc311eeb762e59abe267cab25dc
mbrook14 <mbrook14@kent.edu>
2025-01-20 03:05:06 -0500

    push example 3

---
+++ Project_sql/2_top_paying_jobs_skills.sql
@@ -0,0 +1,35 @@
+
+/*
+Question: What skills are required for the top-paying data analyst jobs?
+- Use the top 10 highest-paying Data Analyst jobs from first query
+- Add the specific skills required for these roles
+- Why? It provides a detailed look at which high-paying jobs demand certain skills, 
+    helping job seekers understand which skills to develop that align with top salaries
+*/
+
+with top_paying_jobs as (
+    SELECT	
+        name AS company_name,
+        salary_year_avg,
+        job_title,
+        job_id
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND 
+        job_location = 'Anywhere' AND 
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    top_paying_jobs.*, --selects all columns from CTE
+    skills
+from top_paying_jobs
+inner join skills_job_dim on top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC;
